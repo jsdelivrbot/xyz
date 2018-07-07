5733c66d8532da9a4dcaa723bf56c4e624ac2dbe
Viktor Tomilin <viktortomilin@gmail.com>
2018-07-07 13:51:40 +0300

    Setup the eslint and fix a code

--- src/env/development/index.js
+++ src/env/development/index.js
@@ -1,17 +1,17 @@
-const nodemon = require('nodemon');
+const nodemon = require('nodemon')
 
 nodemon({
-  script: 'src/env/development/server.js',
-  ext: 'js ejs json',
-  ignore: ["assets/*", "*/bandle.js"],
-});
+	script: 'src/env/development/server.js',
+	ext: 'js ejs json',
+	ignore: ['assets/*', '*/bandle.js'],
+})
 
 nodemon.on('start', function () {
-  process.stdout.write('\033c');
-  console.log('App has started');
+	process.stdout.write('\033c')
+	console.log('App has started')
 }).on('quit', function () {
-  console.log('App has quit');
-  process.exit();
+	console.log('App has quit')
+	process.exit()
 }).on('restart', function (files) {
-  console.log('App restarted due to: ', files);
-});
+	console.log('App restarted due to: ', files)
+})
--- tools/webpack.dev.config.js
+++ tools/webpack.dev.config.js
@@ -1,31 +1,30 @@
-var path = require('path');
-var MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: [
-    "./src/app/assets/javascript/index.js",
-    "./src/app/assets/stylesheets/main.css",
-  ],
-  output: {
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          { loader: MiniCssExtractPlugin.loader },
-          "css-loader"
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css"
-    })
-  ]
-};
+	mode: 'development',
+	devtool: 'inline-source-map',
+	entry: [
+		'./src/app/assets/javascript/index.js',
+		'./src/app/assets/stylesheets/main.css',
+	],
+	output: {
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					{ loader: MiniCssExtractPlugin.loader },
+					'css-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: '[name].css',
+			chunkFilename: '[id].css'
+		})
+	]
+}
